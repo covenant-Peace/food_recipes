d5e7bf4c47e55b0b9c5fd952860a41a1ab314bab
Olubunmi <covenantp4@gmail.com>
2022-08-14 22:30:45 +0100

    commit

--- lib/notification.dart
+++ lib/notification.dart
@@ -1,10 +1,11 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'constants.dart';
 
 class Notification extends StatelessWidget {
-  const Notification({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,11 +13,25 @@ class Notification extends StatelessWidget {
       body: ListView(
         children: [
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Icon(Icons.arrow_back_ios_new_outlined, color: Color(0xfff2f2f2),),
-
+              Icon(
+                Icons.arrow_back_ios_new_outlined,
+                color: Color(0xfff2f2f2),
+              ),
+              SvgPicture.asset('images/pen.svg'),
             ],
-          )
+          ),
+          SizedBox(
+            height: 45.97,
+          ),
+          Text('Notification',
+          style: kTextJourney10,
+          ),
+          SizedBox(
+            height: 35,
+          ),
+
         ],
       ),
     );

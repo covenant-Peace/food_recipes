8e4c0aa0ad6a2057a3d00bfe16e301fa7167e044
Olubunmi <covenantp4@gmail.com>
2022-08-17 19:07:51 +0100

    commit

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -1,10 +1,24 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
-class LandingPage extends StatelessWidget {
-  const LandingPage({Key? key}) : super(key: key);
+class LandingPage extends StatefulWidget {
 
   @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color(0xff222222),
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
   }
 }
+

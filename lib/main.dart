cd9f81495062b1e8bf70498e08c03274dda27ec1
Olubunmi <covenantp4@gmail.com>
2022-08-23 21:25:28 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: JourneyScreen(),
-      // initialRoute: HomeScreen.id,
-      // routes: {
-      //   SignUpScreen.id: (context) => SignUpScreen(),
-      //   JourneyScreen.id: (context) => JourneyScreen(),
-      // },
     );
   }
 }

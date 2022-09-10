40c94c9f9393dd3aff8f46c2baf2cf678dea01da
Olubunmi <covenantp4@gmail.com>
2022-09-10 09:00:34 +0100

    commit

--- lib/delicious.dart
+++ lib/delicious.dart
@@ -1,7 +1,9 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors, must_be_immutable
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:food_recipes/controller/shopping_controller.dart';
+import 'package:get/get.dart';
 
 import 'constants.dart';
 
@@ -11,15 +13,13 @@ enum Food {
   soup,
 }
 
-class Delicious extends StatefulWidget {
+class Delicious extends StatelessWidget {
   // const Delicious({Key? key}) : super(key: key);
 
-  @override
-  State<Delicious> createState() => _DeliciousState();
-}
-
-class _DeliciousState extends State<Delicious> {
   final GlobalKey<ScaffoldState> _gkey = GlobalKey<ScaffoldState>();
+  final controller = Get.put(ShoppingController());
+
+  Food chosen = Food.salad;
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +34,7 @@ class _DeliciousState extends State<Delicious> {
       key: _gkey,
       appBar: AppBar(
         leading: Padding(
-          padding: const EdgeInsets.only(left: 20),
+          padding: const EdgeInsets.only(left: 35),
           child: GestureDetector(
               onTap: () => _gkey.currentState.openDrawer(),
               child: SvgPicture.asset(
@@ -59,7 +59,7 @@ class _DeliciousState extends State<Delicious> {
       ),
       backgroundColor: Color(0xff222222),
       body: Padding(
-        padding: EdgeInsets.only(left: getW(37)),
+        padding: EdgeInsets.only(left: getW(37), right: getW(30)),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -79,87 +79,99 @@ class _DeliciousState extends State<Delicious> {
             ),
             Row(
               children: [
-                GestureDetector(
-                  onTap: () {
-                    // Navigator.push(
-                    //     context,
-                    //     MaterialPageRoute(
-                    //         builder: (context) => BottomNavigation(0)));
-                  },
-                  child: Container(
-                    padding: EdgeInsets.only(
-                        left: getW(18),
-                        right: getW(21),
-                        top: getH(4),
-                        bottom: getH(7)),
-                    height: getH(35),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Color(0xFFEDA92E),
-                    ),
-                    alignment: Alignment.center,
-                    child: Text(
-                      'Salads',
-                      style: kTextJourney29,
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: getW(5),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    // Navigator.push(
-                    //     context,
-                    //     MaterialPageRoute(
-                    //         builder: (context) => BottomNavigation(0)));
-                  },
-                  child: Container(
-                    padding: EdgeInsets.only(
-                        left: getW(18),
-                        right: getW(21),
-                        top: getH(4),
-                        bottom: getH(7)),
-                    height: getH(35),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Color(0xFFEDA92E),
-                    ),
-                    alignment: Alignment.center,
-                    child: Text(
-                      'Vegetables',
-                      style: kTextJourney29,
-                      textAlign: TextAlign.center,
+                Obx(()=> GestureDetector(
+                    onTap: () {
+                      // setState(() {
+                        chosen = Food.salad;
+                      // });
+                      // Navigator.push(
+                      //     context,
+                      //     MaterialPageRoute(
+                      //         builder: (context) => BottomNavigation(0)));
+                    },
+                    child: Container(
+                      padding: EdgeInsets.only(
+                          left: getW(18),
+                          right: getW(21),
+                          top: getH(4),
+                          bottom: getH(7)),
+                      height: getH(35),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: chosen == Food.salad ? Color(0xFFEDA92E): Color(0xfff2f2f2),
+                      ),
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Salads',
+                        style: kTextJourney29,
+                        textAlign: TextAlign.center,
+                      ),
                     ),
                   ),
                 ),
                 SizedBox(
                   width: getW(5),
                 ),
-                GestureDetector(
-                  onTap: () {
-                    // Navigator.push(
-                    //     context,
-                    //     MaterialPageRoute(
-                    //         builder: (context) => BottomNavigation(0)));
-                  },
-                  child: Container(
-                    padding: EdgeInsets.only(
-                        left: getW(18),
-                        right: getW(21),
-                        top: getH(4),
-                        bottom: getH(7)),
-                    height: getH(35),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Color(0xFFEDA92E),
+                Obx(()=> GestureDetector(
+                    onTap: () {
+                      // setState(() {
+                        chosen = Food.vegetables;
+                      // });
+                      // Navigator.push(
+                      //     context,
+                      //     MaterialPageRoute(
+                      //         builder: (context) => BottomNavigation(0)));
+                    },
+                    child: Container(
+                      padding: EdgeInsets.only(
+                          left: getW(18),
+                          right: getW(21),
+                          top: getH(4),
+                          bottom: getH(7)),
+                      height: getH(35),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: chosen == Food.vegetables ? Color(0xFFEDA92E): Color(0xfff2f2f2),
+                      ),
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Vegetables',
+                        style: kTextJourney29,
+                        textAlign: TextAlign.center,
+                      ),
                     ),
-                    alignment: Alignment.center,
-                    child: Text(
-                      'Soup',
-                      style: kTextJourney29,
-                      textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(
+                  width: getW(5),
+                ),
+                Obx(()=> GestureDetector(
+                    onTap: () {
+                      // setState(() {
+                        chosen = Food.soup;
+                      // });
+                      // Navigator.push(
+                      //     context,
+                      //     MaterialPageRoute(
+                      //         builder: (context) => BottomNavigation(0)));
+                    },
+                    child: Container(
+                      padding: EdgeInsets.only(
+                          left: getW(18),
+                          right: getW(21),
+                          top: getH(4),
+                          bottom: getH(7)),
+                      height: getH(35),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: chosen == Food.soup ? Color(0xFFEDA92E): Color(0xfff2f2f2),
+                      ),
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Soup',
+                        style: kTextJourney29,
+                        textAlign: TextAlign.center,
+                      ),
                     ),
                   ),
                 ),
@@ -168,6 +180,17 @@ class _DeliciousState extends State<Delicious> {
                 ),
                 SvgPicture.asset('images/filt.svg')
               ],
+            ),
+            SizedBox(
+              height: getH(25),
+            ),
+            Divider(
+              height: getH(0.2),
+              thickness: getW(0.5),
+              color: Color(0xffe5e5e5),
+            ),
+            SizedBox(
+              height: getH(16),
             )
           ],
         ),
@@ -175,4 +198,5 @@ class _DeliciousState extends State<Delicious> {
     );
   }
 }
+
 

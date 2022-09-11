cf5908dcfb797210007f0034049c55b02da282cf
Olubunmi <covenantp4@gmail.com>
2022-09-11 16:22:15 +0100

    commit

--- lib/auth_service.dart
+++ lib/auth_service.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:food_recipes/constants.dart';
 import 'package:food_recipes/sign_up.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -33,4 +36,50 @@ class AUthService {
       return await FirebaseAuth.instance.signInWithCredential(credential);
     }
   }
+}
+
+class VerifyEmail extends StatefulWidget {
+  // const VerifyEmail({Key? key}) : super(key: key);
+
+  @override
+  State<VerifyEmail> createState() => _VerifyEmailState();
+}
+
+class _VerifyEmailState extends State<VerifyEmail> {
+  bool isVerified = false;
+
+  @override
+  void initState() {
+    super.initState();
+    isVerified = FirebaseAuth.instance.currentUser.emailVerified;
+    if (!isVerified) {
+      sendVerify();
+    }
+  }
+
+  Future sendVerify() async {
+    try {
+      final user = FirebaseAuth.instance.currentUser;
+      await user.sendEmailVerification();
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('$e'),
+        duration: Duration(seconds: 6),
+      ));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) => isVerified
+      ? BottomNavigation(0)
+      : showDialog(
+          context: context,
+          builder: (context) => Dialog(
+                backgroundColor: Colors.transparent,
+                child: Text(
+                  'An email link has been sent to your mail. Please verify!.',
+                  style: kTextJourney31,
+                  textAlign: TextAlign.center,
+                ),
+              ));
 }

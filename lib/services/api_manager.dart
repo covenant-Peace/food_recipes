eeee5bff2bfd4c4bb3ebbec6efe6fbac437bdc8a
Olubunmi <covenantp4@gmail.com>
2022-09-15 22:04:04 +0100

    commit

---
+++ lib/services/api_manager.dart
@@ -0,0 +1,14 @@
+import 'package:food_recipes/strings.dart';
+import 'package:http/http.dart' as http;
+
+
+class API_Manager {
+  void getdata() async{
+    var client = http.Client();
+   var response = await  client.get(Uri.parse(Strings.food_url));
+   if(response.statusCode == 200){
+     var jsonString = response.body;
+   }
+
+  }
+}
---
+++ lib/strings.dart
@@ -0,0 +1,3 @@
+class Strings{
+  static String food_url = 'https://mapp.nairabox.com:8443/api/v1/food/';
+}

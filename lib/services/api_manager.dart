3e41dc4431f38f371250e0678d66bb9d11151c12
Olubunmi <covenantp4@gmail.com>
2022-09-19 22:39:53 +0100

    commit

--- lib/services/api_manager.dart
+++ lib/services/api_manager.dart
@@ -1,14 +1,12 @@
 import 'package:food_recipes/strings.dart';
 import 'package:http/http.dart' as http;
 
-
 class API_Manager {
-  void getdata() async{
+  void getdata() async {
     var client = http.Client();
-   var response = await  client.get(Uri.parse(Strings.food_url));
-   if(response.statusCode == 200){
-     var jsonString = response.body;
-   }
-
+    var response = await client.get(Uri.parse(Strings.food_url));
+    if (response.statusCode == 200) {
+      var jsonString = response.body;
+    }
   }
 }

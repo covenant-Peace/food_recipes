8113bda0322731d7cb544d122e0e811651bc8e47
Olubunmi <covenantp4@gmail.com>
2022-10-08 23:21:07 +0100

    commit

--- lib/paymentFile/payment.dart
+++ lib/paymentFile/payment.dart
@@ -97,157 +97,155 @@ class _PaymentScreenState extends State<PaymentScreen> {
       // ),
     ];
 
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Color(0xFF222222),
-        body: Padding(
-          padding: const EdgeInsets.only(left: 36, right: 36, top: 26),
-          child: ListView(
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => BottomNavigation(2)));
-                      FocusScope.of(context).unfocus();
-                    },
-                    child: Icon(
-                      Icons.keyboard_arrow_left_sharp,
-                      color: Colors.white,
+    return Scaffold(
+      backgroundColor: Color(0xFF222222),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 36, right: 36, top: 26),
+        child: ListView(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => BottomNavigation(2)));
+                    FocusScope.of(context).unfocus();
+                  },
+                  child: Icon(
+                    Icons.keyboard_arrow_left_sharp,
+                    color: Colors.white,
+                  ),
+                ),
+                // SizedBox(
+                //   width: 1.0,
+                // ),
+                Text(
+                  'Payment History',
+                  style: kTextJourney11,
+                ),
+                SizedBox(
+                  width: 1.0,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 35.0,
+            ),
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //   children: <Widget>[
+            //     Text(
+            //       'Saved payment card',
+            //       style: kTextJourney9,
+            //     ),
+            //     GestureDetector(
+            //       onTap: () {
+            //         // controller.cardAdd(history)
+            //         // Get.to(PaymentMethod());
+            //         Navigator.push(
+            //             context,
+            //             MaterialPageRoute(
+            //                 builder: (context) => PaymentMethod()));
+            //       },
+            //       child: Container(
+            //         height: 28.0,
+            //         width: 80.0,
+            //         decoration: BoxDecoration(
+            //           borderRadius: BorderRadius.circular(2.0),
+            //           color: Colors.orange,
+            //         ),
+            //         child: Row(
+            //           children: [
+            //             Icon(
+            //               Icons.add_box_outlined,
+            //               color: Colors.black,
+            //             ),
+            //             Text(
+            //               'Add new',
+            //               style: kTextJourney7,
+            //             ),
+            //           ],
+            //         ),
+            //       ),
+            //     ),
+            //   ],
+            // ),
+            // SizedBox(
+            //   height: 15.0,
+            // ),
+            // Column(
+            //   children: <Widget>[
+            //     foodWidget.first,
+            //     SizedBox(
+            //       height: 15.0,
+            //     ),
+            //     foodWidget.last,
+            //   ],
+            // ),
+            // SizedBox(
+            //   height: 38.0,
+            // ),
+            Text(
+              'History()',
+              style: kTextJourney9,
+            ),
+            Column(
+              children: [
+                SizedBox(
+                  height: 10.0,
+                ),
+                Column(
+                  children: [
+                    HistoryCard(
+                      food: 'Fruit Salad + Ogbono Soup + Small chop ...',
+                      amount: 8000,
                     ),
-                  ),
-                  // SizedBox(
-                  //   width: 1.0,
-                  // ),
-                  Text(
-                    'Payment History',
-                    style: kTextJourney11,
-                  ),
-                  SizedBox(
-                    width: 1.0,
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 35.0,
-              ),
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              //   children: <Widget>[
-              //     Text(
-              //       'Saved payment card',
-              //       style: kTextJourney9,
-              //     ),
-              //     GestureDetector(
-              //       onTap: () {
-              //         // controller.cardAdd(history)
-              //         // Get.to(PaymentMethod());
-              //         Navigator.push(
-              //             context,
-              //             MaterialPageRoute(
-              //                 builder: (context) => PaymentMethod()));
-              //       },
-              //       child: Container(
-              //         height: 28.0,
-              //         width: 80.0,
-              //         decoration: BoxDecoration(
-              //           borderRadius: BorderRadius.circular(2.0),
-              //           color: Colors.orange,
-              //         ),
-              //         child: Row(
-              //           children: [
-              //             Icon(
-              //               Icons.add_box_outlined,
-              //               color: Colors.black,
-              //             ),
-              //             Text(
-              //               'Add new',
-              //               style: kTextJourney7,
-              //             ),
-              //           ],
-              //         ),
-              //       ),
-              //     ),
-              //   ],
-              // ),
-              // SizedBox(
-              //   height: 15.0,
-              // ),
-              // Column(
-              //   children: <Widget>[
-              //     foodWidget.first,
-              //     SizedBox(
-              //       height: 15.0,
-              //     ),
-              //     foodWidget.last,
-              //   ],
-              // ),
-              // SizedBox(
-              //   height: 38.0,
-              // ),
-              Text(
-                'History()',
-                style: kTextJourney9,
-              ),
-              Column(
-                children: [
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  Column(
-                    children: [
-                      HistoryCard(
-                        food: 'Fruit Salad + Ogbono Soup + Small chop ...',
-                        amount: 8000,
-                      ),
-                      HistoryCard(
-                        food: 'Fruit Salad + Ogbono Soup + Small chop ...',
-                        amount: 8000,
-                      ),
-                      HistoryCard(
-                        food: 'Fruit Salad + Ogbono Soup + Small chop ...',
-                        amount: 5000,
-                      ),
-                    ],
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              // GestureDetector(
-              //   onTap: () {
-              //     setState(
-              //       () {
-              //         selectedCard == Paymentcard.first
-              //             ? foodWidget.removeAt(0)
-              //             : foodWidget.removeAt(1);
-              //       },
-              //     );
-              //     // Navigator.push(context,
-              //     //     MaterialPageRoute(builder: (context) => BottomNavigation(2)));
-              //   },
-              //   child: Container(
-              //     width: 341.0,
-              //     height: 56.0,
-              //     decoration: BoxDecoration(
-              //       borderRadius: BorderRadius.circular(48.0),
-              //       color: Color(0xFFEDA92E),
-              //     ),
-              //     alignment: Alignment.center,
-              //     child: Text(
-              //       'Delete Card',
-              //       style: kTextJourney4,
-              //       textAlign: TextAlign.center,
-              //     ),
-              //   ),
-              // ),
-            ],
-          ),
+                    HistoryCard(
+                      food: 'Fruit Salad + Ogbono Soup + Small chop ...',
+                      amount: 8000,
+                    ),
+                    HistoryCard(
+                      food: 'Fruit Salad + Ogbono Soup + Small chop ...',
+                      amount: 5000,
+                    ),
+                  ],
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            // GestureDetector(
+            //   onTap: () {
+            //     setState(
+            //       () {
+            //         selectedCard == Paymentcard.first
+            //             ? foodWidget.removeAt(0)
+            //             : foodWidget.removeAt(1);
+            //       },
+            //     );
+            //     // Navigator.push(context,
+            //     //     MaterialPageRoute(builder: (context) => BottomNavigation(2)));
+            //   },
+            //   child: Container(
+            //     width: 341.0,
+            //     height: 56.0,
+            //     decoration: BoxDecoration(
+            //       borderRadius: BorderRadius.circular(48.0),
+            //       color: Color(0xFFEDA92E),
+            //     ),
+            //     alignment: Alignment.center,
+            //     child: Text(
+            //       'Delete Card',
+            //       style: kTextJourney4,
+            //       textAlign: TextAlign.center,
+            //     ),
+            //   ),
+            // ),
+          ],
         ),
       ),
     );
---
+++ lib/recipe_description.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+import 'bottom_navigation.dart';
+
+class Salad extends StatefulWidget {
+  // const Salad({Key? key}) : super(key: key);
+
+  @override
+  State<Salad> createState() => _SaladState();
+}
+
+class _SaladState extends State<Salad> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff222222),
+      body: Column(
+        children: [
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => BottomNavigation(0)));
+              FocusScope.of(context).unfocus();
+            },
+            child: Icon(
+              Icons.keyboard_arrow_left_sharp,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

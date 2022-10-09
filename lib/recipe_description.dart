2d74ff423fb7f8a291601cd9bc97adbec54a764c
Olubunmi <covenantp4@gmail.com>
2022-10-09 13:13:02 +0100

    commit

--- lib/recipe_description.dart
+++ lib/recipe_description.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 import 'bottom_navigation.dart';
 import 'constants.dart';
@@ -89,8 +90,8 @@ class _SaladState extends State<Salad> {
             ),
             Row(
               children: [
-                Text('2 Eggs'),
-                Text('1/2 Cup Whole Milk')
+                Text('2 Eggs', style: kTextJourney28),
+                Text('1/2 Cup Whole Milk', style: kTextJourney28)
               ],
             ),
             SizedBox(
@@ -98,10 +99,82 @@ class _SaladState extends State<Salad> {
             ),
             Row(
               children: [
-                Text('2 Eggs'),
-                Text('1/2 Cup Whole Milk')
+                Text(
+                  '2 Eggs',
+                  style: kTextJourney28,
+                ),
+                Text('1/2 Cup Whole Milk', style: kTextJourney28)
               ],
-            )
+            ),
+            SizedBox(
+              height: geth(22),
+            ),
+            Text(
+              'Direction',
+              style: kTextJourney14,
+            ),
+            SizedBox(
+              height: geth(3),
+            ),
+            Divider(
+              height: 1,
+              thickness: 1,
+              color: Color(0xff6a6a6a),
+            ),
+            Row(
+              children: [
+                Text(
+                  '•  ',
+                  style: TextStyle(color: Color(0xffeda92e), fontSize: 15),
+                ),
+                Text(
+                  'commodo et sit tortor dolor commodo sed. Felis id integer ut aenean ipsum morbi turpis',
+                  style: kTextJourney28,
+                )
+              ],
+            ),
+            SizedBox(
+              height: geth(11),
+            ),
+            Row(
+              children: [
+                Text(
+                  '•  ',
+                  style: TextStyle(color: Color(0xffeda92e), fontSize: 15),
+                ),
+                Text(
+                  'commodo et sit tortor dolor commodo sed. Felis id integer ut aenean ipsum morbi turpis',
+                  style: kTextJourney28,
+                )
+              ],
+            ),
+            SizedBox(
+              height: geth(11),
+            ),
+            Row(
+              children: [
+                Text(
+                  '•  ',
+                  style: TextStyle(color: Color(0xffeda92e), fontSize: 15),
+                ),
+                Text(
+                  'commodo et sit tortor dolor commodo sed. Felis id integer ut aenean ipsum morbi turpis',
+                  style: kTextJourney28,
+                )
+              ],
+            ),
+            SizedBox(
+              height: geth(28),
+            ),
+            Row(
+              children: [
+                Text('Share via   '),
+                SvgPicture.asset('images/fb.svg'),
+                SvgPicture.asset('images/whatsapp.svg'),
+                SvgPicture.asset('images/twitter.svg'),
+                SvgPicture.asset('images/instagram.svg')
+              ],
+            ),
           ],
         ),
       ),
